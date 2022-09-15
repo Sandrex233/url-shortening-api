892f0a1623b84f13fd7f7ab2acd2351aa95cc90a
Sandro Tsertsvadze <sandrex233@gmail.com>
2022-09-15 16:21:40 +0400

    file+folder structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import React from "react";
 
+import Navbar from './components/Navbar'
+import Header from './components/Header'
+import Api from './components/Api'
+import Features from './components/Features'
+import Boost from './components/Boost'
+import Footer from './components/Footer'
 
 function App() {
   return (
-    <div>
-      
+    <div className="font-psans">
+      <Navbar />
+      <Header />
+      <Api />
+      <Features />
+      <Boost />
+      <Footer />
+
     </div>
   );
 }
---
+++ src/components/Boost.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Boost = () => {
+    return (
+        <div>Boost</div>
+    )
+}
+
+export default Boost
